aa41b4788372d32cdeeeadc0c59547958be36ff5
Marc Schwering <schweringmarc01@gmail.com>
2023-02-13 16:39:46 +0100

    chemistry looks good

--- e1_co2_fixing/README.md
+++ e1_co2_fixing/README.md
@@ -22,3 +22,76 @@ python -m experiments.e1_co2_fixing.main --n_steps=20
 # watch progress on tensorboard
 tensorboard --logdir=./experiments/e1_co2_fixing/runs
 ```
+
+## Chemistry
+
+I wanted to have a diverse set of reactions that would allow different
+possibilities to fix carbon to some biologically useful molecule.
+I started by extracting reactions and molecule species from _Gong, Fuyu & Cai, Zhen & Li, Yin. (2016). Synthetic biology for CO2 fixation_.
+These are 6 carbon fixing pathways.
+Molecules _G3P_, _acetyl-CoA_, and _pyruvate_ are the carbon carriers that the cell will consume to replicate.
+I added a molecule species _X_ and reactions for how these carbon carriers can be converted to _X_.
+With _X_ I have a single handle that can be used during the simulation.
+
+Energy is provided by _NADPH_ and _ATP_.
+If a reaction needed an electron donor I explicitly added $\text{NADPH} \rightleftharpoons \text{NADP}$.
+If it needed a phophate group I explicitly added $\text{ATP} \rightleftharpoons \text{ADP}$.
+Other reactions are defined without energy carriers, so that the cell can create its own way of driving the reaction.
+To restore energy carriers I added a molecule species _Y_ and reactions with _Y_ to restore _NADPH_ and _ATP_.
+Similar to _X_, _Y_ can be used as a handle to restore energy during the simulation.
+
+The pathways already overlap in some molecule species.
+I would have liked to add more crosslinks between those pathways (_e.g._ can't hydroxybutyrate be converted to malate?).
+But I don't know enough about possible reactions.
+Below is the current list of reactions with reaction energies.
+
+$$
+\begin{align*}
+\text{Y} + 2 \; \text{ADP} & \rightleftharpoons 2 \; \text{ATP} \; & (-80 \; \text{kJ/mol}) \\
+\text{Y} + \text{NADP} & \rightleftharpoons \text{NADPH} \; & (-80 \; \text{kJ/mol}) \\
+\text{G3P} & \rightleftharpoons 8 \; \text{X} \; & (-20 \; \text{kJ/mol}) \\
+\text{pyruvate} & \rightleftharpoons 6 \; \text{X} \; & (-30 \; \text{kJ/mol}) \\
+\text{acetyl-CoA} & \rightleftharpoons \text{HS-CoA} + 5 \; \text{X} \; & (-35 \; \text{kJ/mol}) \\
+\text{NADPH} & \rightleftharpoons \text{NADP} \; & (-70 \; \text{kJ/mol}) \\
+\text{ATP} & \rightleftharpoons \text{ADP} \; & (-35 \; \text{kJ/mol}) \\
+\text{CO2} + \text{RuBP} & \rightleftharpoons 2 \; \text{3PGA} \; & (-35 \; \text{kJ/mol}) \\
+\text{ATP} + \text{3PGA} & \rightleftharpoons \text{1,3BPG} + \text{ADP} \; & (-15 \; \text{kJ/mol}) \\
+\text{NADPH} + \text{1,3BPG} & \rightleftharpoons \text{G3P} + \text{NADP} \; & (-20 \; \text{kJ/mol}) \\
+5 \; \text{G3P} & \rightleftharpoons 3 \; \text{Ru5P} \; & (-15 \; \text{kJ/mol}) \\
+\text{Ru5P} + \text{ATP} & \rightleftharpoons \text{RuBP} + \text{ADP} \; & (-5 \; \text{kJ/mol}) \\
+\text{CO2} + \text{NADPH} & \rightleftharpoons \text{formate} + \text{NADP} \; & (-10 \; \text{kJ/mol}) \\
+\text{FH4} + \text{formate} & \rightleftharpoons \text{formyl-FH4} \; & (25 \; \text{kJ/mol}) \\
+\text{formyl-FH4} + \text{NADPH} & \rightleftharpoons \text{methylen-FH4} + \text{NADP} \; & (-10 \; \text{kJ/mol}) \\
+\text{NADPH} + \text{methylen-FH4} & \rightleftharpoons \text{methyl-FH4} + \text{NADP} \; & (-15 \; \text{kJ/mol}) \\
+\text{CO2} + \text{NADPH} & \rightleftharpoons \text{CO} + \text{NADP} \; & (-5 \; \text{kJ/mol}) \\
+\text{HS-CoA} + \text{methyl-FH4} + \text{CO} & \rightleftharpoons \text{FH4} + \text{acetyl-CoA} \; & (0 \; \text{kJ/mol}) \\
+\text{CO2} + \text{acetyl-CoA} & \rightleftharpoons \text{malonyl-CoA} \; & (10 \; \text{kJ/mol}) \\
+3 \; \text{NADPH} + \text{malonyl-CoA} & \rightleftharpoons \text{propionyl-CoA} + 3 \; \text{NADP} \; & (-30 \; \text{kJ/mol}) \\
+\text{CO2} + \text{propionyl-CoA} & \rightleftharpoons \text{methylmalonyl-CoA} \; & (0 \; \text{kJ/mol}) \\
+\text{methylmalonyl-CoA} & \rightleftharpoons \text{succinyl-CoA} \; & (0 \; \text{kJ/mol}) \\
+\text{succinyl-CoA} & \rightleftharpoons \text{HS-CoA} + \text{succinate} \; & (-10 \; \text{kJ/mol}) \\
+\text{succinate} + \text{NADP} & \rightleftharpoons \text{NADPH} + \text{fumarate} \; & (0 \; \text{kJ/mol}) \\
+\text{fumarate} & \rightleftharpoons \text{malate} \; & (0 \; \text{kJ/mol}) \\
+\text{HS-CoA} + \text{malate} & \rightleftharpoons \text{malyl-CoA} \; & (10 \; \text{kJ/mol}) \\
+\text{malyl-CoA} & \rightleftharpoons \text{glyoxylate} + \text{acetyl-CoA} \; & (0 \; \text{kJ/mol}) \\
+\text{propionyl-CoA} + \text{glyoxylate} & \rightleftharpoons \text{methylmalyl-CoA} \; & (-5 \; \text{kJ/mol}) \\
+\text{methylmalyl-CoA} & \rightleftharpoons \text{citramalyl-CoA} \; & (0 \; \text{kJ/mol}) \\
+\text{citramalyl-CoA} & \rightleftharpoons \text{pyruvate} + \text{acetyl-CoA} \; & (-5 \; \text{kJ/mol}) \\
+\text{NADPH} + \text{oxalacetate} & \rightleftharpoons \text{malate} + \text{NADP} \; & (-5 \; \text{kJ/mol}) \\
+\text{malate} & \rightleftharpoons \text{fumarate} \; & (0 \; \text{kJ/mol}) \\
+\text{NADPH} + \text{fumarate} & \rightleftharpoons \text{succinate} + \text{NADP} \; & (0 \; \text{kJ/mol}) \\
+\text{HS-CoA} + \text{succinate} & \rightleftharpoons \text{succinyl-CoA} \; & (10 \; \text{kJ/mol}) \\
+\text{CO2} + \text{NADPH} + \text{succinyl-CoA} & \rightleftharpoons \text{alpha-ketoglutarate} + \text{HS-CoA} + \text{NADP} \; & (-35 \; \text{kJ/mol}) \\
+\text{alpha-ketoglutarate} + \text{CO2} + \text{NADPH} & \rightleftharpoons \text{isocitrate} + \text{NADP} \; & (-20 \; \text{kJ/mol}) \\
+\text{isocitrate} & \rightleftharpoons \text{citrate} \; & (0 \; \text{kJ/mol}) \\
+\text{HS-CoA} + \text{citrate} & \rightleftharpoons \text{oxalacetate} + \text{acetyl-CoA} \; & (35 \; \text{kJ/mol}) \\
+\text{CO2} + \text{NADPH} + \text{acetyl-CoA} & \rightleftharpoons \text{HS-CoA} + \text{pyruvate} + \text{NADP} \; & (-35 \; \text{kJ/mol}) \\
+\text{pyruvate} + \text{ATP} & \rightleftharpoons \text{PEP} + \text{ADP} \; & (-15 \; \text{kJ/mol}) \\
+\text{CO2} + \text{PEP} & \rightleftharpoons \text{oxalacetate} \; & (-10 \; \text{kJ/mol}) \\
+\text{NADPH} + \text{succinyl-CoA} & \rightleftharpoons \text{HS-CoA} + \text{SSA} + \text{NADP} \; & (-30 \; \text{kJ/mol}) \\
+\text{NADPH} + \text{SSA} & \rightleftharpoons \text{GHB} + \text{NADP} \; & (-5 \; \text{kJ/mol}) \\
+\text{HS-CoA} + \text{GHB} & \rightleftharpoons \text{hydroxybutyryl-CoA} \; & (35 \; \text{kJ/mol}) \\
+\text{hydroxybutyryl-CoA} + \text{NADP} & \rightleftharpoons \text{acetoacetyl-CoA} + \text{NADPH} \; & (5 \; \text{kJ/mol}) \\
+\text{HS-CoA} + \text{acetoacetyl-CoA} & \rightleftharpoons 2 \; \text{acetyl-CoA} \; & (0 \; \text{kJ/mol}) \\
+\end{align*}
+$$
--- e1_co2_fixing/chemistry.py
+++ e1_co2_fixing/chemistry.py
@@ -60,7 +60,7 @@ methyl-FH4 + CO + HS-CoA -> acetyl-CoA + FH4
 oxalacetate + NADPH -> malate + NADP (-64.2 kJ/mol)
 malate -> fumarate (+2.3 kJ/mol)
 fumarate + FADH -> succinate + FAD (-102.2 kJ/mol)
-succinate + HS-CoA + ATP -> succinyl-CoA + ADP 
+succinate + HS-CoA + ATP -> succinyl-CoA + ADP
 succinyl-CoA + FdRed + CO2 -> a-ketoglutarate + HS-CoA + FdOx (-14.8 kJ/mol incl prev)
 a-ketoglutarate + CO2 + NADPH -> isocitrate + NADP (+3.8 kJ/mol)
 isocitrate -> citrate (-8.5 kJ/mol)
@@ -68,7 +68,7 @@ citrate + HS-CoA + ATP -> oxalacetate + acetyl-CoA + ADP
 
 ## Dicarboxylate/4-Hydroxybutyrate Cycle
 
-acetyl-CoA + Fd-red -> pyruvate + Fd-ox
+acetyl-CoA + Fd-red -> pyruvate + Fd-ox + HS-CoA
 pyruvate + ATP -> PEP + AMP
 PEP + CO2 -> oxalacetate
 oxalacetate + NADPH -> malate + NADP
@@ -112,58 +112,72 @@ E.g. no FADH2 or Fd-red is defined.
 Molecule X will be used to caputure biologically available carbon.
 Molecule Y will be used to replenish energy carriers.
 
-acetyl-CoA -> HS-CoA + 2 X
-G3P -> 3 X
-pyruvate -> 3 X
-NADP + Y -> NADPH
-3 ADP + Y -> 3 ATP
+## Finding Energies
+
+1. decided to have NADPH->NADP (-70), ATP->ADP (-30) to match real data
+2. decided on some base energy for carriers (e.g. HS-CoA=200)
+3. for each metabolite counted number of Ps, Cs, and C-C/C-H bonds
+4. created initial energies for metabolites by multiplying these counts:
+   20 * [P] + 10 * [C] + 60 * [C-C/C-H]
+5. started adjusting these initial energies to yield the real reaction
+   energies I found in publications (e.g. in WL and reductive TCA pathway)
+6. result is a tradeoff by iterative trial-and-error to come close to
+   real reaction energies but also be consistent across all pathways
+7. derived energies and reactions for X and Y to be as fair as possible
+   for any carrier molecule
+
+I would like to add more transformations that are not described in these
+pathway but I don't know enough about this topic.
+E.g. shoudln't there be a hydroxybutyrate <-> malate (or similar)?
 """
+from collections import Counter
 from magicsoup.containers import Molecule, Chemistry
-
-
-# TODO
-# aufsetzen auf HS-CoA: ATP -> ADP (-20)
-# CO2 auf CoA mit aufnehmen: ATP -> ADP (-20)
-# 2e reduzieren: NADPH -> NADP (-50)
-
-# TODO: propionyl-CoA + CO2 -> methylmalonyl-CoA (0 kJ/mol) is weird
-
-
-def f(p: int, c: int, e: int) -> float:
-    return 20 * p + 10 * c + 30 * e
-
 
 _co2 = Molecule("CO2", 10.0 * 1e3, diffusivity=1.0, permeability=1.0)
 _NADPH = Molecule("NADPH", 200.0 * 1e3)
 _NADP = Molecule("NADP", 130.0 * 1e3)
 _ATP = Molecule("ATP", 100.0 * 1e3)
 _ADP = Molecule("ADP", 65.0 * 1e3)
-_G3P = Molecule("G3P", 420.0 * 1e3)  # P 3C 12e !
-_acetylCoA = Molecule("acetyl-CoA", 475.0 * 1e3)  # 2C 8e !
-_HSCoA = Molecule("HS-CoA", 200.0 * 1e3)  # !
-_pyruvate = Molecule("pyruvate", 900 * 1e3)  # 3C 10e
-_X = Molecule("X", 250.0 * 1e3)  # TODO
+_G3P = Molecule("G3P", 420.0 * 1e3)  # P 3C 12e
+_acetylCoA = Molecule("acetyl-CoA", 475.0 * 1e3)  # 2C 8e
+_HSCoA = Molecule("HS-CoA", 190.0 * 1e3)
+_pyruvate = Molecule("pyruvate", 330.0 * 1e3)  # 3C 10e
+_X = Molecule("X", 50.0 * 1e3)
 _Y = Molecule("Y", 150.0 * 1e3)
 
-_common_mols = [_co2, _NADPH, _NADP, _ATP, _ADP]
-
 _common_reacts = [
+    # energy carriers with defined reaction energies
     ([_NADPH], [_NADP]),  # -70
     ([_ATP], [_ADP]),  # -35
+    # restoring them should be practically irreversible
     ([_ADP, _ADP, _Y], [_ATP, _ATP]),  # -80
     ([_NADP, _Y], [_NADPH]),  # -80
-    ([_G3P], [_X, _X, _X]),  # TODO
-    ([_pyruvate], [_X, _X, _X]),  # TODO
-    ([_acetylCoA], [_HSCoA, _X, _X]),  # TODO
+    # extracting carbon should be as fair as possible for all
+    ([_G3P], [_X, _X, _X, _X, _X, _X, _X, _X]),  # -20
+    ([_pyruvate], [_X, _X, _X, _X, _X, _X]),  # -30
+    ([_acetylCoA], [_HSCoA, _X, _X, _X, _X, _X]),  # -35
 ]
 
+_common_mols = [
+    _co2,
+    _NADPH,
+    _NADP,
+    _ATP,
+    _ADP,
+    _acetylCoA,
+    _HSCoA,
+    _pyruvate,
+    _G3P,
+    _X,
+    _Y,
+]
 
 # Calvin
-_RuBP = Molecule("RuBP", 725.0 * 1e3)  # 2P 5C 20e !
-_3PGA = Molecule("3PGA", 350.0 * 1e3)  # P 3C 10e !
-_13BPG = Molecule("1,3BPG", 370.0 * 1e3)  # 2P 3C 10e !
-_G3P = Molecule("G3P", 420.0 * 1e3)  # P 3C 12e !
-_Ru5P = Molecule("Ru5P", 695.0 * 1e3)  # P 5C 20e !
+_RuBP = Molecule("RuBP", 725.0 * 1e3)  # 2P 5C 20e
+_3PGA = Molecule("3PGA", 350.0 * 1e3)  # P 3C 10e
+_13BPG = Molecule("1,3BPG", 370.0 * 1e3)  # 2P 3C 10e
+_G3P = Molecule("G3P", 420.0 * 1e3)  # P 3C 12e
+_Ru5P = Molecule("Ru5P", 695.0 * 1e3)  # P 5C 20e
 
 _calvin_reacts = [
     ([_RuBP, _co2], [_3PGA, _3PGA]),  # -35
@@ -178,7 +192,7 @@ _calvin_mols = [_RuBP, _3PGA, _13BPG, _G3P, _Ru5P]
 
 # 3-Hydroxypropionate bicycle
 _acetylCoA = Molecule("acetyl-CoA", 475.0 * 1e3)  # 2C 8e !
-_HSCoA = Molecule("HS-CoA", 200.0 * 1e3)  # !
+_HSCoA = Molecule("HS-CoA", 190.0 * 1e3)
 _malonylCoA = Molecule("malonyl-CoA", 495.0 * 1e3)  # 3C 8e !
 _propionylCoA = Molecule("propionyl-CoA", 675.0 * 1e3)  # 3C 14e !
 _methylmalonylCoA = Molecule("methylmalonyl-CoA", 685.0 * 1e3)  # 4C 14e !
@@ -200,10 +214,10 @@ _hprop_reacts = [
     ),  # -30
     ([_propionylCoA, _co2], [_methylmalonylCoA]),  # 0
     ([_methylmalonylCoA], [_succinylCoA]),  # 0
-    ([_succinylCoA], [_succinate, _HSCoA]),  # 0
+    ([_succinylCoA], [_succinate, _HSCoA]),  # -10
     ([_succinate, _NADP], [_fumarate, _NADPH]),  # 0
     ([_fumarate], [_malate]),  # 0
-    ([_malate, _HSCoA], [_malylCoA]),  # 0
+    ([_malate, _HSCoA], [_malylCoA]),  # +10
     ([_malylCoA], [_acetylCoA, _glyoxylate]),  # 0
     ([_propionylCoA, _glyoxylate], [_methylmalylCoA]),  # -5
     ([_methylmalylCoA], [_citramalylCoA]),  # 0
@@ -233,7 +247,7 @@ _FH4 = Molecule("FH4", 200.0 * 1e3)  # !
 _formate = Molecule("formate", 70.0 * 1e3)  # 1C 2e !
 _co = Molecule("CO", 75 * 1e3)  # 1C 2e !
 _acetylCoA = Molecule("acetyl-CoA", 475.0 * 1e3)  # 2C 8e !
-_HSCoA = Molecule("HS-CoA", 200.0 * 1e3)  # !
+_HSCoA = Molecule("HS-CoA", 190.0 * 1e3)
 
 _wl_reacts = [
     ([_co2, _NADPH], [_formate, _NADP]),  # -10
@@ -241,7 +255,7 @@ _wl_reacts = [
     ([_formylFH4, _NADPH], [_methylenFH4, _NADP]),  # -10
     ([_methylenFH4, _NADPH], [_methylFH4, _NADP]),  # -15
     ([_co2, _NADPH], [_co, _NADP]),  # -5
-    ([_methylFH4, _co, _HSCoA], [_acetylCoA, _FH4]),  # -10
+    ([_methylFH4, _co, _HSCoA], [_acetylCoA, _FH4]),  # 0
 ]
 
 _wl_mols = [
@@ -255,7 +269,7 @@ _wl_mols = [
 
 # Reductive TCA
 _acetylCoA = Molecule("acetyl-CoA", 475.0 * 1e3)  # 2C 8e !
-_HSCoA = Molecule("HS-CoA", 200.0 * 1e3)  # !
+_HSCoA = Molecule("HS-CoA", 190.0 * 1e3)
 _oxalacetate = Molecule("oxalacetate", 350.0 * 1e3)  # 4C 10e !
 _malate = Molecule("malate", 415.0 * 1e3)  # 4C 12e !
 _fumarate = Molecule("fumarate", 415.0 * 1e3)  # 4C 12e !
@@ -269,11 +283,11 @@ _rtca_reacts = [
     ([_oxalacetate, _NADPH], [_malate, _NADP]),  # -5
     ([_malate], [_fumarate]),  # 0
     ([_fumarate, _NADPH], [_succinate, _NADP]),  # 0
-    ([_succinate, _HSCoA], [_succinylCoA]),  # 0
-    ([_succinylCoA, _NADPH, _co2], [_aKetoglutarate, _HSCoA, _NADP]),  # -25
+    ([_succinate, _HSCoA], [_succinylCoA]),  # +10
+    ([_succinylCoA, _NADPH, _co2], [_aKetoglutarate, _HSCoA, _NADP]),  # -35
     ([_aKetoglutarate, _co2, _NADPH], [_isocitrate, _NADP]),  # -20
     ([_isocitrate], [_citrate]),  # 0
-    ([_citrate, _HSCoA], [_oxalacetate, _acetylCoA]),  # +25
+    ([_citrate, _HSCoA], [_oxalacetate, _acetylCoA]),  # +35
 ]
 
 _rtca_mols = [
@@ -298,22 +312,22 @@ _succinate = Molecule("succinate", 485.0 * 1e3)  # 4C 14e !
 _succinylCoA = Molecule("succinyl-CoA", 685.0 * 1e3)  # 4C 14e !
 _SSA = Molecule("SSA", 535.0 * 1e3)  # 4C 16e !
 _GHB = Molecule("GHB", 600.0 * 1e3)  # 4C 18e !
-_hydroxybutyrylCoA = Molecule("hydroxybutyryl-CoA", 830.0 * 1e3)  # 4C 18e !
+_hydroxybutyrylCoA = Molecule("hydroxybutyryl-CoA", 825.0 * 1e3)
 _acetoacetylCoA = Molecule("acetoacetyl-CoA", 760.0 * 1e3)  # 4C 16e !
 
 _dcarbhb_reacts = [
-    ([_acetylCoA, _co2, _NADPH], [_pyruvate, _HSCoA, _NADP]),  # -25
+    ([_acetylCoA, _co2, _NADPH], [_pyruvate, _HSCoA, _NADP]),  # -35
     ([_pyruvate, _ATP], [_PEP, _ADP]),  # -15
     ([_PEP, _co2], [_oxalacetate]),  # -10
     ([_oxalacetate, _NADPH], [_malate, _NADP]),  # -5
     ([_malate], [_fumarate]),  # 0
     ([_fumarate, _NADPH], [_succinate, _NADP]),  # 0
-    ([_succinate, _HSCoA], [_succinylCoA]),  # 0
-    ([_succinylCoA, _NADPH], [_SSA, _HSCoA, _NADP]),  # -20
+    ([_succinate, _HSCoA], [_succinylCoA]),  # +10
+    ([_succinylCoA, _NADPH], [_SSA, _HSCoA, _NADP]),  # -30
     ([_SSA, _NADPH], [_GHB, _NADP]),  # -5
-    ([_GHB, _HSCoA], [_hydroxybutyrylCoA]),  # +30
-    ([_hydroxybutyrylCoA, _NADP], [_acetoacetylCoA, _NADPH]),  # 0
-    ([_acetoacetylCoA, _HSCoA], [_acetylCoA, _acetylCoA]),  # -10
+    ([_GHB, _HSCoA], [_hydroxybutyrylCoA]),  # +35
+    ([_hydroxybutyrylCoA, _NADP], [_acetoacetylCoA, _NADPH]),  # +5
+    ([_acetoacetylCoA, _HSCoA], [_acetylCoA, _acetylCoA]),  # 0
 ]
 
 _dcarbhb_mols = [
@@ -331,15 +345,15 @@ _dcarbhb_mols = [
 ]
 
 # 3-Hydroxypropionate/4-Hydroxybutyrate Cycle
-_acetylCoA = Molecule("acetyl-CoA", 475.0 * 1e3)  # 2C 8e !
-_malonylCoA = Molecule("malonyl-CoA", 495.0 * 1e3)  # 3C 8e !
-_propionylCoA = Molecule("propionyl-CoA", 675.0 * 1e3)  # 3C 14e !
-_methylmalonylCoA = Molecule("methylmalonyl-CoA", 685.0 * 1e3)  # 4C 14e !
-_succinylCoA = Molecule("succinyl-CoA", 685.0 * 1e3)  # 4C 14e !
-_SSA = Molecule("SSA", 535.0 * 1e3)  # 4C 16e !
-_GHB = Molecule("GHB", 600.0 * 1e3)  # 4C 18e !
-_hydroxybutyrylCoA = Molecule("hydroxybutyryl-CoA", 830.0 * 1e3)  # 4C 18e !
-_acetoacetylCoA = Molecule("acetoacetyl-CoA", 760.0 * 1e3)  # 4C 16e !
+_acetylCoA = Molecule("acetyl-CoA", 475.0 * 1e3)
+_malonylCoA = Molecule("malonyl-CoA", 495.0 * 1e3)
+_propionylCoA = Molecule("propionyl-CoA", 675.0 * 1e3)
+_methylmalonylCoA = Molecule("methylmalonyl-CoA", 685.0 * 1e3)
+_succinylCoA = Molecule("succinyl-CoA", 685.0 * 1e3)
+_SSA = Molecule("SSA", 535.0 * 1e3)
+_GHB = Molecule("GHB", 600.0 * 1e3)
+_hydroxybutyrylCoA = Molecule("hydroxybutyryl-CoA", 825.0 * 1e3)
+_acetoacetylCoA = Molecule("acetoacetyl-CoA", 760.0 * 1e3)
 
 _hprophbut_reacts = [
     ([_acetylCoA, _co2], [_malonylCoA]),  # +10
@@ -349,11 +363,11 @@ _hprophbut_reacts = [
     ),  # -30
     ([_propionylCoA, _co2], [_methylmalonylCoA]),  # 0
     ([_methylmalonylCoA], [_succinylCoA]),  # 0
-    ([_succinylCoA, _NADPH], [_SSA, _HSCoA, _NADP]),  # -20
+    ([_succinylCoA, _NADPH], [_SSA, _HSCoA, _NADP]),  # -30
     ([_SSA, _NADPH], [_GHB, _NADP]),  # -5
-    ([_GHB, _HSCoA], [_hydroxybutyrylCoA]),  # +30
-    ([_hydroxybutyrylCoA, _NADP], [_acetoacetylCoA, _NADPH]),  # 0
-    ([_acetoacetylCoA, _HSCoA], [_acetylCoA, _acetylCoA]),  # -10
+    ([_GHB, _HSCoA], [_hydroxybutyrylCoA]),  # +35
+    ([_hydroxybutyrylCoA, _NADP], [_acetoacetylCoA, _NADPH]),  # +5
+    ([_acetoacetylCoA, _HSCoA], [_acetylCoA, _acetylCoA]),  # 0
 ]
 
 _hprophbut_mols = [
@@ -368,7 +382,6 @@ _hprophbut_mols = [
 ]
 
 # combined
-
 MOLECULES = (
     _common_mols
     + _calvin_mols
@@ -390,3 +403,25 @@ REACTIONS = (
 )
 
 CHEMISTRY = Chemistry(molecules=MOLECULES, reactions=REACTIONS)
+
+
+def print_mathjax(chem: Chemistry):
+    print(r"\begin{align*}")
+    for subs, prods in chem.reactions:
+        sub_cnts = Counter(r"\text{" + d.name + r"}" for d in subs)
+        prod_cnts = Counter(r"\text{" + d.name + r"}" for d in prods)
+        sub_strs = [("" if d < 2 else rf"{d} \; ") + k for k, d in sub_cnts.items()]
+        prod_strs = [("" if d < 2 else rf"{d} \; ") + k for k, d in prod_cnts.items()]
+        sub_str = " + ".join(sub_strs)
+        prod_str = " + ".join(prod_strs)
+        raw_energy = sum(d.energy for d in prods) - sum(d.energy for d in subs)
+        fmd_energy = f"{raw_energy / 1e3:.0f}" + r" \; \text{kJ/mol}"
+        print(
+            sub_str
+            + r" & \rightleftharpoons "
+            + prod_str
+            + r" \; & "
+            + f"({fmd_energy})"
+            + r" \\"
+        )
+    print(r"\end{align*}")
